779ed92869f5faeebfe0db904cda3e7fe1aca735
OgurecRoman <treninasonya@yandex.ru>
2024-11-30 17:52:03 +0300

    fix some bugs

--- demo/src/main/java/com/example/demo/FaceRecognition.java
+++ demo/src/main/java/com/example/demo/FaceRecognition.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
